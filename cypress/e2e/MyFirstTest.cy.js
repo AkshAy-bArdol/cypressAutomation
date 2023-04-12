6abb32dab84b596b086944ded82465c8703a1576
Akshay <bardolakshay@gmail.com>
2023-04-12 11:27:03 +0530

    new code

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,14 @@
+
+
+describe('My First Test', () => {
+    it('Test1', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("[name='username']").type("Admin")
+        cy.get("[name='password']").type("admin123")
+        cy.get("[type='submit']").click()
+        cy.title().contains("Orang")
+        
+        
+    })
+
+})
